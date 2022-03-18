8b6d46f11cdc5ae4f517e67accb2ed4cb37df7e4
vladislav bachmackiy <astrax200469@gmail.com>
2022-03-18 21:06:00 +0300

    feat: added parsing of dto's

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppDto } from './app.dto';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post()
+  suka(@Body() appDto: AppDto): AppDto {
+    return appDto;
+  }
 
   @Get()
   getHello(): string {
---
+++ src/app.dto.ts
@@ -0,0 +1,5 @@
+export class AppDto {
+  id: number;
+
+  name: string;
+}
